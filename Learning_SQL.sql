d1d17a5f0c477f16c8f5758ccc26bee4962e0cc8
Wambui Mungai <ijoy.mungai@gmail.com>
2021-06-08 13:44:10 +1000

    update new queries

--- Learning_SQL.sql
+++ Learning_SQL.sql
@@ -6,4 +6,35 @@ SELECT DISTINCT CITY
 FROM
 STATION
 WHERE 
-CITY RLIKE '^[aeiou]' AND CITY RLIKE '[aeiou]$'  
+CITY RLIKE '^[aeiou]' AND CITY RLIKE '[aeiou]$'; 
+
+
+SELECT 
+DISTINCT CITY
+FROM
+STATION
+WHERE
+CITY NOT RLIKE '^[aeiou]' OR CITY NOT RLIKE '[aeiou]$';
+
+SELECT DISTINCT  ct.company_Code, ct.founder,  
+    COUNT(DISTINCT lmt.lead_manager_code) as total_lead_manager,  
+    COUNT(DISTINCT smt.senior_manager_code) as total_senior_manager,  
+    COUNT(DISTINCT mt.manager_code) as total_manager, 
+    COUNT( DISTINCT et.employee_code) as total_employee  
+FROM Company ct  
+LEFT JOIN Lead_Manager lmt  
+ON lmt.company_code = ct.company_code  
+LEFT JOIN Senior_Manager smt  
+ON smt.lead_manager_code = lmt.lead_manager_code  
+AND ct.company_code = lmt.company_code  
+LEFT JOIN Manager mt 
+ON mt.senior_manager_code = smt.senior_manager_code 
+AND mt.lead_manager_code = lmt.lead_manager_code
+AND mt.company_code = ct.company_code 
+LEFT JOIN Employee et 
+ON et.manager_code = mt.manager_code 
+AND et.senior_manager_code = smt.senior_manager_code 
+AND et.lead_manager_code = lmt.lead_manager_code 
+AND et.company_code = ct.company_code 
+GROUP BY ct.company_Code, ct.founder 
+ORDER BY Company_Code ASC ; 
