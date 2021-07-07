d3ce7bc91c44aa874df2af7809dc24822ff90161
Wambui Mungai <ijoy.mungai@gmail.com>
2021-07-07 11:08:24 +1000

    advanced select queries

--- Learning_SQL.sql
+++ Learning_SQL.sql
@@ -16,6 +16,8 @@ STATION
 WHERE
 CITY NOT RLIKE '^[aeiou]' OR CITY NOT RLIKE '[aeiou]$';
 
+
+/* New merged company*/
 SELECT DISTINCT  ct.company_Code, ct.founder,  
     COUNT(DISTINCT lmt.lead_manager_code) as total_lead_manager,  
     COUNT(DISTINCT smt.senior_manager_code) as total_senior_manager,  
@@ -38,3 +40,52 @@ AND et.lead_manager_code = lmt.lead_manager_code
 AND et.company_code = ct.company_code 
 GROUP BY ct.company_Code, ct.founder 
 ORDER BY Company_Code ASC ; 
+
+/*oracle sql
+Pivot the Occupation column
+*/
+select D, P, S, A
+  from (
+        Select Row_Number() OVER (PARTITION BY occupation ORDER BY name) rownumber, 
+               name,
+               occupation       
+          from occupations
+       ) t
+pivot(
+  min(name)
+  for occupation in ('Doctor' D, 'Professor' P, 'Singer' S, 'Actor' A)
+)
+order by rownumber;
+
+/* Mysql
+alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical
+*/
+SELECT CONCAT(NAME, "(", LEFT(OCCUPATION,1) ,")") as NAME
+FROM OCCUPATIONS
+ORDER BY NAME;
+
+/*
+Query the number of ocurrences of each occupation in OCCUPATIONS.
+*/
+
+SELECT CONCAT("There are a total of"," ", COUNT(OCCUPATION), " ", LOWER(OCCUPATION), "s.") FROM OCCUPATIONS GROUP BY OCCUPATION ORDER BY COUNT(OCCUPATION)
+
+/* Mysql 
+Type of Triangle*/
+SELECT 
+CASE WHEN  A + B <= C OR B + C <= A AND C + A <= B THEN "Not A Triangle"
+     WHEN A = B AND B = C AND C = A THEN  "Equilateral"
+     WHEN A = B OR B = C OR C = A THEN "Isosceles"
+     ELSE "Scalene"
+END
+FROM TRIANGLES
+
+/*
+Binary Tree Nodes
+*/
+SELECT N, CASE WHEN P IS NULL THEN 'Root'
+               WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+               ELSE 'Leaf'
+               END
+FROM BST
+ORDER BY N
